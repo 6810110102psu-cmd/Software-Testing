6d360c81c7b37bb52eef2567086c32f7985c33df
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-22 16:15:04 +0000

    Init project structure for 5 problems

--- hackerrang/1_funny_string.py
+++
@@ -1,8 +0,0 @@
-def funnyString(s):
-    r = s[::-1]  # reverse string
-
-    for i in range(1, len(s)):
-        if abs(ord(s[i]) - ord(s[i - 1])) != abs(ord(r[i]) - ord(r[i - 1])):
-            return "Not Funny"
-
-    return "Funny"
