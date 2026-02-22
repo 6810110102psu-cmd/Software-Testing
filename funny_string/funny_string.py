42ebc841fb01294126bf68b9c33c72017d1038cf
6810110102psu-cmd <6810110102psu@gmail.com>
2026-02-22 16:20:00 +0000

    Implement Funny String logic

---
+++ funny_string/funny_string.py
@@ -0,0 +1,12 @@
+def funny_string(s): # สร้างสตริงย้อนกลับ
+    r = s[::-1]
+    n = len(s)
+    
+    for i in range(1, n): 
+        diff_s = abs(ord(s[i]) - ord(s[i-1]))
+        diff_r = abs(ord(r[i]) - ord(r[i-1]))
+        
+        if diff_s != diff_r:
+            return "Not Funny"
+            
+    return "Funny"
